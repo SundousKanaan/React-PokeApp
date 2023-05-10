493956c5404ae2aba8f4e1a1f97605e145a87897
Sylwia Werner <sylwia.werner@wearetriple.com>
2023-05-10 09:25:32 +0000

    Revert "Merged PR 17093: Add pokemon list
    - Cleanup
    - Add pokemon list
    - Add media queries for main layout"

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,21 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { routes } from "@app/routes";
 import $ from "./app.module.scss";
-import { Sidebar } from "./components/organisms/sidebar/Sidebar";
 
-const App = () => {
+const App: React.FunctionComponent = () => {
   return (
+    /**
+     * By creating a classname with $ it can be defined in app.module.scss
+     * It will be suffixed with a hash in the build (like .app__HK5) to prevent
+     * that other components with the same classname can effect the styling as well
+     */
     <main className={$.app}>
       <div className={$.view}>
-        <Sidebar />
+        <section className={$.sidebar}>
+          <h2>Sidebar</h2>
+        </section>
+        {/* Create a list by mapping over all the pokemon names and rendering the Pokemon Component */}
         <Routes>
           {routes.map(({ path, view: Component }) => (
             <Route key={path} path={path} element={<Component />} />
--- src/components/organisms/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import $ from "./Header.module.scss";
-
-export const Header = () => (
-  <header className={$.header}>
-    // TODO: add hamburger menu and logo
-    <div>hamburger menu</div>
-    <div>logo</div>
-  </header>
-);
